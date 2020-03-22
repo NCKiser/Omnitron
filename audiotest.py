3277bff6be8bcabd8fb59fa3dbad6e1eef1591ce
Noah Stephen Haskell <nstephenh@gmail.com>
2020-03-22 17:03:51 -0400

    Began messing with midi imports

---
+++ audiotest.py
@@ -0,0 +1,79 @@
+import mido
+import pygame
+import pygame.midi
+import time
+import random
+
+screen_width = 640
+screen_height = 480
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+pygame.init()
+
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.SCALED, 32)
+
+block_list = pygame.sprite.Group()
+all_sprites_list = pygame.sprite.Group()
+laser_sprites = pygame.sprite.Group()
+
+# Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+score = 0
+
+state = 0
+level_loading = 0
+
+audio_track = None
+playhead = 0
+pygame.midi.init()
+while not pygame.midi.get_init():
+    pass #Wait for midi library to initialize
+midi_out = pygame.midi.Output()
+
+# -------- Main Program Loop -----------
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    # Clear the screen
+    screen.fill(WHITE)
+    if state == 0:
+        # MENU
+        if pygame.mouse.get_pressed():
+            print("Mouse Clicked")
+            state = 1
+            level_loading = 1
+    if state == 1:
+        if level_loading == 1:
+            print("LOADING LEVEL")
+            audio_track = mido.MidiFile("music/abridge_d.mid").tracks[1]
+            playhead = 0
+            level_loading = 0
+        else:
+            pass
+            if playhead < len(audio_track):
+                midi_out.write(audio_track[playhead])
+                print(audio_track[playhead])
+                playhead += 1
+            else:
+                print("Audio track ended)")
+
+
+
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+    # Limit to 60 frames per second
+    clock.tick(60)
+
+pygame.quit()
+
+pygame.display.flip()
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Omnitron
+By 
+Noah Kiser
+Noah Haskell
+Brendan Noah Freeze
+
+Asset Design by
+Noah Kiser
+and
+Cassie Paulin
+
+
+Music:
+http://www.partnersinrhyme.com/midi/Dance_Techno/Dance_Techno_Midi_Files/abridge_d.shtml
