9a045285fccd7d6cf7e7c4c51dce073f49a7366e
Noah Stephen Haskell <nstephenh@gmail.com>
2020-04-01 19:10:32 -0400

    basic import script

--- engine.py
+++ engine.py
@@ -153,7 +153,7 @@ for key in enemy_tracks:
 level_state = 0
 menu = 0
 level_start = 0
-level_name = "level1.csv"
+level_name = "moonlightSonata.txt.csv"
 level = 1
 player_difficulty = 2
 # -------- Main Program Loop -----------
@@ -168,7 +168,7 @@ while not done:
                 for row in csv.reader(level_file):
                     try:
                         if row[0] != 'appear_time' and row[0] != '#' and row[0] != '//':
-                            appear_time = int(row[0])
+                            appear_time = float(row[0])
                             key = int(row[1]) % KEYS
                             instrument = row[2].strip()
                             note = row[3].strip()
---
+++ import_music.py
@@ -0,0 +1,34 @@
+import argparse
+import csv
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input")
+args = parser.parse_args()
+
+print(args.input)
+output_file_name = args.input + ".csv"
+os.remove(output_file_name)
+output_file = open(output_file_name, 'w')
+output_csv = csv.writer(output_file)
+
+instrument_map = []
+notes = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
+
+
+with open(args.input) as input_file:
+
+    commands = input_file.read().split('|')[-1].split(';')
+    for command in commands:
+        try:
+            items = command.split(' ')
+            time = items[0]
+            note = items[1].lower()
+            length = items[2]
+            instrument = items[3]
+            key = notes.index(note[0])
+            output = [time, key, 'piano', note, 2]
+            output_csv.writerow(output)
+        except IndexError as e:
+            print(e)
+
